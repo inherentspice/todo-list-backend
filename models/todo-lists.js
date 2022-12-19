3ad3108e0a1b489df32bbb225979e3c7d8ab8456
Stephen Smith <stephenlehanesmith@gmail.com>
2022-12-19 12:32:10 +0800

    added new mongoose model for todolists

---
+++ models/todo-lists.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
+  })
+
+
+const todoListsSchema = new mongoose.Schema({
+  content: String,
+  toggled: Boolean,
+});
+
+todoListsSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+});
+
+module.exports = mongoose.model('TodoLists', todoListsSchema);
